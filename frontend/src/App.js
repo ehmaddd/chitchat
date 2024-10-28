88eee2df3459c6d8d3b2727b134e6f69f98956e4
ehmaddd <ehmaddd@gmail.com>
2024-10-28 11:42:25 +0500

    Setup basic nav logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
+      <Navbar />
+    </>
   );
 }
 
