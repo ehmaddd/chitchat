c23634c4424ef200dd69857bb0414ce240cbfd95
ehmaddd <ehmaddd@gmail.com>
2024-10-28 12:23:28 +0500

    Setup Login module / install authentication libraries

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,9 +5,7 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
+      <Link to="/signup">SignUp</Link>
     </nav>
   );
 }
