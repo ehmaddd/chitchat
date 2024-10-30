208f6bcef5dc6bdcae5490d1166e796579a158d5
ehmaddd <ehmaddd@gmail.com>
2024-10-30 15:52:55 +0500

    Fixx Login module display issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Login />
     </div>
   );
 }
