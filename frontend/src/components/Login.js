ffc511e7182eb85ca57d5508c9422d7838ccd070
ehmaddd <ehmaddd@gmail.com>
2024-10-31 14:47:10 +0500

    Add Login button

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <h1>Login</h1>
+        <>
+          <h1>Login</h1>
+          <form>
+              <label for="email">Email : </label>
+              <input type="text" name="email"></input>
+              <label for="password">Email : </label>
+              <input type="password" name="email"></input>
+              <input type="submit">Login</input>
+          </form>
+        </>
     )
 }
 
