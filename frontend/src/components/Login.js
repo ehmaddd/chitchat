8245c8f59d846629b5309cfb6a9b1e2c18ff25ab
ehmaddd <ehmaddd@gmail.com>
2024-11-04 12:02:16 +0500

    Update Login module for state

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <>
           <h1>Login</h1>
           <form>
-              <label for="email">Email : </label>
-              <input type="text" name="email"></input>
-              <label for="password">Email : </label>
-              <input type="password" name="email"></input>
-              <input type="submit">Login</input>
+              <label htmlFor="email">Email: </label>
+              <input type="text" name="email" onChange={(e) => setEmail(e.target.value)} />
+              <label htmlFor="password">Password: </label>
+              <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} />
+              <input type="submit" value="Login" />
           </form>
         </>
-    )
-}
+    );
+};
 
 export default Login;
