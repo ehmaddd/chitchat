e580396b3311031e6ac01150a065f43c68419a3a
ehmaddd <ehmaddd@gmail.com>
2024-11-11 12:33:19 +0500

    Log username & password

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -4,15 +4,22 @@ const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const handleSubmit = (e)=> {
+        e.preventDefault();
+        console.log(email, password);
+    }
+
     return (
         <>
           <h1>Login</h1>
           <form>
               <label htmlFor="email">Email: </label>
               <input type="text" name="email" onChange={(e) => setEmail(e.target.value)} />
+              <br></br>
               <label htmlFor="password">Password: </label>
               <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} />
-              <input type="submit" value="Login" />
+              <br></br>
+              <input type="submit" value="Login" onClick={(e)=>handleSubmit(e)} />
           </form>
         </>
     );
