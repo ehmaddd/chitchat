99a40989828e03f37177377d6f5f6407b302cf22
ehmaddd <ehmaddd@gmail.com>
2024-12-09 11:47:37 +0500

    Update styling of login message

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './Login.css';
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [result, setResult] = useState('');
 
     const handleSubmit = async (e)=> {
       e.preventDefault();
@@ -12,12 +14,21 @@ const Login = () => {
           email,
           password
       });
-      console.log(response.data);
+      if(response.data.id){
+        setResult('Login Successful');
+      }
+      else {
+        setResult('Login Failed');
+      }
     }
+
+    const backgroundColor =
+    result === 'Login Successful' ? 'green' : result === 'Login Failed' ? 'red' : 'transparent';
 
     return (
         <>
           <h1>Login</h1>
+          <p className="message" style={{ backgroundColor }}>{result}</p>
           <form>
               <label htmlFor="email">Email: </label>
               <input type="text" name="email" onChange={(e) => setEmail(e.target.value)} />
