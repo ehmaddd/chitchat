6e90ca70de5925f5120e680c0e580904e6bf5c8a
ehmaddd <ehmaddd@gmail.com>
2024-12-23 12:14:58 +0500

    Add LogOut Button

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './Login.css';
+import Chat from './Chat';
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [result, setResult] = useState('');
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     const handleSubmit = async (e)=> {
       e.preventDefault();
@@ -17,29 +19,39 @@ const Login = () => {
         });
         if(response.data.id){
           setResult('Login Successful');
+          setIsLoggedIn(true);
           console.log(response.data);
         }
         else {
           setResult('Login Failed');
+          setIsLoggedIn(false);
         }
       } catch {
         setResult('Login Failed');
+        setIsLoggedIn(false);
       }
     }
 
     return (
         <>
+          {!isLoggedIn ? (
+          <>
           <h1>Login</h1>
           <p className={(result==='Login Successful')?'message-success':'message-failure'} >{result}</p>
-          <form>
-              <label htmlFor="email">Email: </label>
-              <input type="text" name="email" onChange={(e) => setEmail(e.target.value)} />
-              <br></br>
-              <label htmlFor="password">Password: </label>
-              <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} />
-              <br></br>
-              <input type="submit" value="Login" onClick={(e)=>handleSubmit(e)} />
-          </form>
+            <form>
+                <label htmlFor="email">Email: </label>
+                <input type="text" name="email" onChange={(e) => setEmail(e.target.value)} />
+                <br></br>
+                <label htmlFor="password">Password: </label>
+                <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} />
+                <br></br>
+                <input type="submit" value="Login" onClick={(e)=>handleSubmit(e)} />
+                <button onClick={logout}>LogOut</button>
+            </form>
+          </>
+          ) : (
+            <Chat />
+          )}
         </>
     );
 };
